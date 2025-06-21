dc424e7800634aa23b15f5dc0b94510322298257
krig6 <ej.ldsm@gmail.com>
2025-06-21 19:00:56 +0800

    test(caesarCipher): Add test for multiple lowercase shifting
    - `caesarCipher('shift',1)` should return 'tigju'

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -3,3 +3,7 @@ import { caesarCipher } from "../src/caesarCipher";
 it("shifts a single letter correctly", () => {
   expect(caesarCipher('a', 1)).toBe('b')
 })
+
+it("shifts multiple lowercase letters correctly", () => {
+  expect(caesarCipher('shift', 1)).toBe('tijgu')
+})
