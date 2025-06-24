b38306c45c149432fae695c89adb9878873ef23c
krig6 <ej.ldsm@gmail.com>
2025-06-25 07:14:51 +0800

    test(analyzeArray): Add initial failing test for array analysis
    - Add test case that expects `analyzeArray` to return object with
    average, min, max, and length properties

---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,10 @@
+import { analyzeArray } from "../src/analyzeArray";
+
+it("correctly analyzes an array and returns average, min, max, and length", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  })
+})
