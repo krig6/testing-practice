e5c4ed225c12a120fef1cc1d7e28d3986ed6de47
krig6 <ej.ldsm@gmail.com>
2025-06-27 13:21:43 +0800

    refactor(caesarCipher): Improve code structure and readability
    - Extract magic number 26 into `ALPHABET_LENGTH` constant
    - Consolidate character type checking with `isLowerCase/isUpperCase`
    variables
    - Restructure conditional logic to use single if-else
    - Reduce code duplication in alphabet shifting logic using ternary
    operator
    - Maintain identical functionality while improving readability

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -4,22 +4,25 @@ export const caesarCipher = (str, shift) => {
   validateInput('caesarCipher', str, 'string')
   validateInput('caesarCipher', shift, 'number')
 
+  const ALPHABET_LENGTH = 26
   const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-
   const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
   const result = []
-  const effectiveShift = ((shift % 26) + 26) % 26
+  const effectiveShift = ((shift % ALPHABET_LENGTH) + ALPHABET_LENGTH) % ALPHABET_LENGTH
 
   for (let char of str) {
-    if (LOWERCASE_LETTERS.includes(char)) {
-      result.push(LOWERCASE_LETTERS[(LOWERCASE_LETTERS.indexOf(char) + effectiveShift) % 26])
-    }
-    if (UPPERCASE_LETTERS.includes(char)) {
-      result.push(UPPERCASE_LETTERS[(UPPERCASE_LETTERS.indexOf(char) + effectiveShift) % 26])
-    }
-    if (!UPPERCASE_LETTERS.includes(char) && !LOWERCASE_LETTERS.includes(char)) {
+    const isLowerCase = LOWERCASE_LETTERS.includes(char)
+    const isUpperCase = UPPERCASE_LETTERS.includes(char)
+
+    if (!isLowerCase && !isUpperCase) {
       result.push(char)
+    }
+    else {
+      result.push(isLowerCase
+        ? LOWERCASE_LETTERS[(LOWERCASE_LETTERS.indexOf(char) + effectiveShift) % ALPHABET_LENGTH]
+        : UPPERCASE_LETTERS[(UPPERCASE_LETTERS.indexOf(char) + effectiveShift) % ALPHABET_LENGTH]
+      )
     }
   }
 
